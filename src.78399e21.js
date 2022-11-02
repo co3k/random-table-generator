0f119f213f9624119b6f673a255b4fd7c5b4d249
co3k <co3k@users.noreply.github.com>
2022-11-02 02:55:42 +0000

    deploy: b5ff3f07971920216d0b43ef478ac872af427e48

--- src.78399e21.js
+++ src.78399e21.js
@@ -29425,7 +29425,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "localhost" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36863" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35455" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
